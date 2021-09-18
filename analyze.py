e732b9f6a143779ee08a92845176925370ded500
Salahuddin-dat <61401962+Salahuddin-dat@users.noreply.github.com>
2021-09-18 23:35:05 +0100

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -14,7 +14,7 @@ a_g_model = TfLiteModel(a_g_model_path)
 
 class GenerateVideo(object):
     def __init__(self):
-        self.video = cv2.VideoCapture(1)
+        self.video = cv2.VideoCapture(-1)
 
     def __del__(self):
         self.video.release()
